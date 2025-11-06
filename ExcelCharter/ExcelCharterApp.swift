6ce5fd7fe4931946f2e779e66f914bc4388df6b1
t <tmont091@gmail.com>
2025-11-05 19:39:57 -0800

    Initial Commit

---
+++ ExcelCharter/ExcelCharterApp.swift
@@ -0,0 +1,17 @@
+//
+//  ExcelCharterApp.swift
+//  ExcelCharter
+//
+//  Created by Paradis d'Abbadon on 05.11.25.
+//
+
+import SwiftUI
+
+@main
+struct ExcelCharterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
