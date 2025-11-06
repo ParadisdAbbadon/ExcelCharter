00815ce64811636f5641a092ba835993f9e5bbd9
t <tmont091@gmail.com>
2025-11-05 20:51:15 -0800

    Architectural changes

---
+++ ExcelCharter/Views/HomeView.swift
@@ -0,0 +1,41 @@
+//
+//  ContentView.swift
+//  ExcelCharter
+//
+//  Created by Paradis d'Abbadon on 05.11.25.
+//
+
+import SwiftUI
+
+struct HomeView: View {
+    // MARK: - Properties
+    private class FileReference { }
+    
+    let sheetfiles = [
+            SheetFile(id: UUID(), title: "Sheet 1"),
+            SheetFile(id: UUID(), title: "Sheet 2"),
+            SheetFile(id: UUID(), title: "Sheet 3")
+        ]
+    
+    // MARK: - Body
+    var body: some View {
+        NavigationStack{
+            List {
+                ForEach(sheetfiles) { sheetfile in
+                    NavigationLink {
+                        //Destination View
+                        Text("Temporary")
+                    } label: {
+                        Text(sheetfile.title)
+                            .font(.title2)
+                    }
+                }
+            }
+            .navigationTitle("Home")
+        }
+    }
+}
+// MARK: - Preview
+#Preview {
+    HomeView()
+}
