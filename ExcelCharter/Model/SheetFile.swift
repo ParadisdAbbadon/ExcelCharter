75ba02be5b2faad1075d8458c02059327bbe5aa9
t <tmont091@gmail.com>
2025-11-11 20:15:06 -0800

    Merged homeimport branch to main

--- ExcelCharter/Model/SheetFile.swift
+++ ExcelCharter/Model/SheetFile.swift
@@ -14,11 +14,9 @@ struct SheetFile: Identifiable {
     let id: UUID
     var title: String
     var url: URL?
-    // Spreadsheet data stored in 2D array (data)
     var data: [[String]]?
     
     init(id: UUID, title: String, data: [[String]]? = nil) {
-        //custom initializer to allow for data to be nil
            self.id = id
            self.title = title
            self.data = data
